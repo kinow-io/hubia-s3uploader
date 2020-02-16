ef8ed9c2e6d2548dfc073d20e94c0c754d6fa3d0
Philippe Sang <philippe@kinow.com>
2020-02-16 21:49:36 +0100

    Updated backend process

--- src/backend/credentials-sdk-v3.php
+++ src/backend/credentials-sdk-v3.php
@@ -7,7 +7,7 @@ use Aws\Sts\StsClient;
 $accessKeyId = 'YOUR_ACCESS_KEY';
 $secretAccessKey = 'YOUR_SECRET_KEY';
 
-$key = 'YOUR_FILENAME';
+$key = $_POST['key'];
 $bucket = 'YOUR_BUCKET';
 $folder = 'YOUR_FOLDER';
 $region = 'YOUR_REGION'; // us-west-1, eu_west-2, etc...
@@ -16,9 +16,8 @@ $signatureVersion = 'YOUR_SIGNATURE_VERSION'; // v2, v3 or v4
 $roleArn = 'arn:aws:iam::xxxxxxxx:role/YOUR_ROLE_NAME';
 $apiVersion = '2011-06-15';
 
-// AWS PHP SDK with STS service to create temporary credentials with a policy
+// Setup policy
 $path = $bucket.'/'.$folder;
-
 $policy = json_encode([
     'Version' => '2012-10-17',
     'Statement' => [
@@ -32,22 +31,31 @@ $policy = json_encode([
     ],
 ]);
 
-$client = StsClient::factory(array(
-    'credentials' => array(
+// Generate temporary credentials
+$client = StsClient::factory([
+    'credentials' => [
         'key'    => $accessKeyId,
         'secret' => $secretAccessKey
-    ),
+    ],
     'apiVersion' => $apiVersion,
     'signatureVersion' => $signatureVersion,
     'region' => $region
-));
-
-$result = $client->assumeRole(array(
+]);
+$result = $client->assumeRole([
     'RoleArn' => $roleArn,
     'RoleSessionName' => 'test',
     'Policy' => $policy,
     'DurationSeconds' => 3600,
-))->get('Credentials');
+])->get('Credentials');
 
 $result['Key'] = $key;
 echo json_encode($result);
+
+die(json_encode([
+    'region' => $region,
+    'bucket' => $bucket,
+    'Key' => $key,
+    'AccessKeyId' => $result['AccessKeyId'],
+    'SecretAccessKey' => $result['SecretAccessKey'],
+    'SessionToken' => $result['SessionToken']
+]));
